19e7bffa8072d2c281b914b808c0aa596e92ec4f
Yaroslav Prokhorovych <93757015+LifeOfPunk@users.noreply.github.com>
2025-08-30 22:32:36 +0700

    Update faq.screen.js

--- src/screens/faq.screen.js
+++ src/screens/faq.screen.js
@@ -40,7 +40,7 @@ export const faqScreen = async (ctx, editMessage) => {
         editMessage,
         text: message,
         reply_markup,
-        photoCandidates: ['src/data/faq_REMOVE.jpg'],
+        photoCandidates: ['src/data/freePrompts.jpg'],
         parse_mode: 'HTML',
         disable_web_page_preview: true,
     });
