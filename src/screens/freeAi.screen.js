55b9b317248fe07bd192585c1882bf5783fd52f8
Yaroslav Prokhorovych <93757015+LifeOfPunk@users.noreply.github.com>
2025-08-30 20:14:03 +0700

    Update freeAi.screen.js

--- src/screens/freeAi.screen.js
+++ src/screens/freeAi.screen.js
@@ -1,14 +1,23 @@
 import 'dotenv/config';
 import { sendOrEdit } from '../utils/media.js';
 
-const keyboard = [[{ text: '⏪ Вернуться назад', command: 'back' }]];
+const keyboard = [
+    [{ text: '😎 Получить 10 нейронок', command: 'ai_link' }],
+    [{ text: '⏪ Вернуться назад', command: 'back' }],
+];
 
 export const freeAiScreen = async (ctx, editMessage) => {
-    const message = `😎 Топ 10 бесплатных нейронок 2025 года подарок текст`;
+    const message = `Мы подготовили ТОП-10 бесплатных нейронок 2025 года. Это твой первый шаг к созданию крутого ИИ-контента без вложений.`;
 
     const reply_markup = {
         inline_keyboard: keyboard.map((row) =>
             row.map((item) => {
+                if (item.command === 'ai_link') {
+                    return {
+                        text: item.text,
+                        url: process.env.FREE_AI_URL,
+                        };
+                }
                 return {
                     text: item.text,
                     callback_data: JSON.stringify({ command: item.command }),
